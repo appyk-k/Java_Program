e002cf29a72a02941e6b666cefb1f7860639879f
Appyk-k <apurvakathvate@gmail.com>
2024-10-04 18:00:11 +0530

    all files are added to Github

--- src/Oct03_2024/Task3_Calculator.java
+++ src/Oct03_2024/Task3_Calculator.java
@@ -1,4 +1,11 @@
 package Oct03_2024;
 
 public class Task3_Calculator {
+    public static void main(String[] args) {
+        int a=40, b=20;
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+    }
 }
