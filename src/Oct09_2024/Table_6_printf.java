f37bac80d49be4e74f62456a60ccd288fabb7b73
Appyk-k <apurvakathvate@gmail.com>
2024-10-10 17:27:32 +0530

    all files are added to Github

---
+++ src/Oct09_2024/Table_6_printf.java
@@ -0,0 +1,20 @@
+package Oct09_2024;
+
+public class Table_6_printf {
+    public static void main(String[] args) {
+
+                // Using printf() to print the table of 2
+                System.out.printf("2 x 1 = %d\n", 2 * 1);
+                System.out.printf("2 x 2 = %d\n", 2 * 2);
+                System.out.printf("2 x 3 = %d\n", 2 * 3);
+                System.out.printf("2 x 4 = %d\n", 2 * 4);
+                System.out.printf("2 x 5 = %d\n", 2 * 5);
+                System.out.printf("2 x 6 = %d\n", 2 * 6);
+                System.out.printf("2 x 7 = %d\n", 2 * 7);
+                System.out.printf("2 x 8 = %d\n", 2 * 8);
+                System.out.printf("2 x 9 = %d\n", 2 * 9);
+                System.out.printf("2 x 10 = %d\n", 2 * 10);
+
+
+    }
+}
